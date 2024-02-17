a35113fab99b25f622132cd2bdc7ed9c3f66aba6
Mariano SY <138610830+marianos1988@users.noreply.github.com>
2024-02-16 23:45:17 -0300

    Implementar botones play y stop en videos

--- js/home.js
+++ js/home.js
@@ -25,30 +25,77 @@ var swiper = new Swiper(".swiper", {
   // Videos About us
 
   const playVideos = () => {
-    allButtons = document.querySelectorAll(".btn-video");
-    allButtons.forEach(element => {
+    allPlaysButtons = document.querySelectorAll(".btn-play-video");
+    allPlaysButtons.forEach(element => {
       element.addEventListener("click",()=>{
-        if(element.id === "btn-video-1") {
+        if(element.id === "btn-play-video-1") {
             document.querySelector("#video-1").play();
-            document.querySelector("#btn-video-1").classList.replace("play","stop");
+            document.querySelector(`#btn-play-video-1`).classList.replace("btn-visible","btn-invisible");
+            document.querySelector("#btn-stop-video-1").classList.replace("btn-invisible","btn-visible");
+
+            document.querySelector("#btn-stop-video-1").addEventListener("click",()=>{
+              document.querySelector("#video-1").pause();
+              document.querySelector("#video-1").currentTime = 0;
+              document.querySelector(`#btn-play-video-1`).classList.replace("btn-invisible","btn-visible");
+              document.querySelector("#btn-stop-video-1").classList.replace("btn-visible","btn-invisible");
+            });
+
             document.querySelector("#video-1").addEventListener("ended", ()=> {
-            document.querySelector("#btn-video-1").classList.replace("stop","play");
+              document.querySelector(`#btn-play-video-1`).classList.replace("btn-invisible","btn-visible");
+              document.querySelector("#btn-stop-video-1").classList.replace("btn-visible","btn-invisible");
+            });
+
+        }
+        else if(element.id === "btn-play-video-2") {
+          document.querySelector("#video-2").play();
+          document.querySelector(`#btn-play-video-2`).classList.replace("btn-visible","btn-invisible");
+          document.querySelector("#btn-stop-video-2").classList.replace("btn-invisible","btn-visible");
+
+          document.querySelector("#btn-stop-video-2").addEventListener("click",()=>{
+            document.querySelector("#video-2").pause();
+            document.querySelector("#video-2").currentTime = 0;
+            document.querySelector(`#btn-play-video-2`).classList.replace("btn-invisible","btn-visible");
+            document.querySelector("#btn-stop-video-2").classList.replace("btn-visible","btn-invisible");
           });
 
-          if(document.querySelector("#btn-video-1").classList.contains("stop")) {
-
-          }
-
-
+          document.querySelector("#video-2").addEventListener("ended", ()=> {
+            document.querySelector(`#btn-play-video-2`).classList.replace("btn-invisible","btn-visible");
+            document.querySelector("#btn-stop-video-2").classList.replace("btn-visible","btn-invisible");
+          });
         }
-        else if(element.id === "btn-video-2") {
-          
+        else if(element.id === "btn-play-video-3") {
+          document.querySelector("#video-3").play();
+          document.querySelector(`#btn-play-video-3`).classList.replace("btn-visible","btn-invisible");
+          document.querySelector("#btn-stop-video-3").classList.replace("btn-invisible","btn-visible");
+
+          document.querySelector("#btn-stop-video-3").addEventListener("click",()=>{
+            document.querySelector("#video-3").pause();
+            document.querySelector("#video-3").currentTime = 0;
+            document.querySelector(`#btn-play-video-3`).classList.replace("btn-invisible","btn-visible");
+            document.querySelector("#btn-stop-video-3").classList.replace("btn-visible","btn-invisible");
+          });
+
+          document.querySelector("#video-3").addEventListener("ended", ()=> {
+            document.querySelector(`#btn-play-video-3`).classList.replace("btn-invisible","btn-visible");
+            document.querySelector("#btn-stop-video-3").classList.replace("btn-visible","btn-invisible");
+          });
         }
-        else if(element.id === "btn-video-3") {
-          
-        }
-        else if(element.id === "btn-video-4") {
-          
+        else if(element.id === "btn-play-video-4") {
+          document.querySelector("#video-4").play();
+          document.querySelector(`#btn-play-video-4`).classList.replace("btn-visible","btn-invisible");
+          document.querySelector("#btn-stop-video-4").classList.replace("btn-invisible","btn-visible");
+
+          document.querySelector("#btn-stop-video-4").addEventListener("click",()=>{
+            document.querySelector("#video-4").pause();
+            document.querySelector("#video-4").currentTime = 0;
+            document.querySelector(`#btn-play-video-4`).classList.replace("btn-invisible","btn-visible");
+            document.querySelector("#btn-stop-video-4").classList.replace("btn-visible","btn-invisible");
+          });
+
+          document.querySelector("#video-4").addEventListener("ended", ()=> {
+            document.querySelector(`#btn-play-video-4`).classList.replace("btn-invisible","btn-visible");
+            document.querySelector("#btn-stop-video-4").classList.replace("btn-visible","btn-invisible");
+          });
         }
       });
     });
